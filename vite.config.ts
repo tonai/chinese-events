46991022880af52893b7ddc03e3544e9fbc8c250
Tony CABAYE <tocab@PT-ASN-SHURO.dhcp.idf.intranet>
2024-04-26 19:04:21 +0200

    feat: add capacitor

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/chinese-events/",
+  base: process.env.BASE_URL ?? "/chinese-events/",
   build: {
     assetsDir: "code",
     cssMinify: true,
