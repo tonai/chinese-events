c6108e187232f14679e784a8782fb5af0006537f
Tony CABAYE <tony.cabaye@smile.fr>
2024-04-29 10:13:51 +0200

    fix: icon color

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,6 +15,7 @@ export default defineConfig({
   },
   plugins: [
     react(),
+    svgr(),
     VitePWA({
       devOptions: {
         // enabled: true,
